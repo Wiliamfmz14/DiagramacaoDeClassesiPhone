48be265b8944eaa0dc248165e02c4bcb668887d5
Wiliamfmz14 <132028874+Wiliamfmz14@users.noreply.github.com>
2025-01-07 07:56:36 -0500

    Create NavegadorDeInternet.java
    adding NavegadorDeInternet interface
    features exibirPagina, atualizarPagina, AdicionarPagina

---
+++ NavegadorDeInternet.java
@@ -0,0 +1,7 @@
+package DesafioClassesiPhone;
+
+public interface NavegadorDeInternet {
+    public void exibirPagina();
+    public void atualizarPagina();
+    public void adicionarNovaPagina();
+}
